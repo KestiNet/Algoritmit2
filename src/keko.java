24ddaf2347eb51e9fc35fc9f1618feb6302224b0
esakesti <esakesti@gmail.com>
2021-09-20 22:47:16 +0100

    compiles without issues now

--- src/keko.java
+++ src/keko.java
@@ -1,22 +1,30 @@
 public class keko {
 
-    public static void lisaaKekoon(int[] kLuvut, int alkio) {
-        if (a[0] >= MAXPQ) {
-            //Taulukko täynnä,
-            //kasvatetaan kokoa tai heitetään poikkeus
+
+        public static void korjaaKeko(int[] kLuvut) {
+            int i = 0;
+            if (2*i > kLuvut[0]) // i:llä ei lapsia,
+                return;       // ei tehdä mitään
+            int j = 2*i;
+            int alkio = kLuvut[i];
+            // Siirretään alkiota kohti lehtisolmuja
+            while (j <= kLuvut[0])
+            {
+                if ((j < kLuvut[0]) && (kLuvut[j] > kLuvut[j+1]))
+                    j = j+1;
+                if (alkio <= kLuvut[j])
+                    break; // lopetetaan silmukka
+                kLuvut[j/2] = kLuvut[j];
+                j = 2*j;
+            }
+            kLuvut[j/2] = alkio;
         }
-        a[0]++;
-        int i = a[0];
-        while ((i > 1) && (a[i / 2] > alkio)) {
-            a[i] = a[i / 2];
-            i = i / 2;
-        }
-        i = i / 2;
-    }
 
     public static void main(String[] args) {
         int[] kLuvut = {1, 5, 4, 6, 7, 6, 8, 321, 4, 2, 87, 213, 6};
 
-        lisaaKekoon(kLuvut);
+        korjaaKeko(kLuvut);
     }
-}
+ }
+
+
